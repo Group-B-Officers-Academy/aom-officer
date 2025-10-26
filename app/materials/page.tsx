fc2026512dd5db70610bcbb3359b465c7b37ed6e
aom-officer <groupbofficersacademy@gmail.com>
2025-10-26 07:29:15 +0530

    Materials added

--- app/materials/page.tsx
+++ app/materials/page.tsx
@@ -4,7 +4,12 @@ const Materials = () => {
 
   return (
     <div>
-      <h1>Materials</h1>
+      <h1>Materials List</h1>
+      <ul>
+        <li>Material 1</li>
+        <li>Material 2</li>
+        <li>Material 3</li>
+      </ul>
     </div>
   )
 }
